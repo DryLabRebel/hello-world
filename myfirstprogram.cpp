86bc43402962bf158be8d68538340c5f15eab9b0
Geoff English <geoffrey.english@uqconnect.edu.au>
2017-11-13 13:16:45 +1000

    Add files via upload

---
+++ myfirstprogram.cpp
@@ -0,0 +1,25 @@
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!";
+  }
+
+// 1. Comment
+// 2. Directive
+// 3. Function
+// 'main' - the function called when the program is run
+// - is it necessary?
+// all c++ programs begin execution via 'main' function
+// 4. {} braces contain function definitions
+// 5. Statement - some expression which produces some effect
+//
+// std::cout - Standard character output (the screen)
+//
+// << the following is to be inserted into std::cout
+//
+// "Hello world" - Text string to be inserted into std::cout
+//
+// ; - marks the end of a statement (like full stop in english) possibly one of the most common sytax errors in c++ is forgetting the ';'
+
